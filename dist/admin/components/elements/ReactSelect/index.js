b5a3ac4672778886a2880b33f1f31986e3f60de6
astranex <astranex.agency@gmail.com>
2022-10-04 16:02:29 +0300

    another console log

--- dist/admin/components/elements/ReactSelect/index.js
+++ dist/admin/components/elements/ReactSelect/index.js
@@ -1,6 +1,10 @@
 import React, { useCallback } from 'react';
-import Select, { components, } from 'react-select';
-import { SortableContainer, SortableElement, SortableHandle, } from 'react-sortable-hoc';
+import Select, { components } from 'react-select';
+import {
+    SortableContainer,
+    SortableElement,
+    SortableHandle
+} from 'react-sortable-hoc';
 import { arrayMove } from '../../../../utilities/arrayMove';
 import Chevron from '../../icons/Chevron';
 import './index.scss';
@@ -13,45 +17,98 @@ const SortableMultiValue = SortableElement((props) => {
         e.preventDefault();
         e.stopPropagation();
     };
-    const classes = [
-        props.className,
-        !props.isDisabled && 'draggable',
-    ].filter(Boolean).join(' ');
-    return (React.createElement(components.MultiValue, { ...props, className: classes, innerProps: { ...props.innerProps, onMouseDown } }));
+    const classes = [props.className, !props.isDisabled && 'draggable']
+        .filter(Boolean)
+        .join(' ');
+    return React.createElement(components.MultiValue, {
+        ...props,
+        className: classes,
+        innerProps: { ...props.innerProps, onMouseDown }
+    });
 });
-const SortableMultiValueLabel = SortableHandle((props) => React.createElement(components.MultiValueLabel, { ...props }));
+const SortableMultiValueLabel = SortableHandle((props) =>
+    React.createElement(components.MultiValueLabel, { ...props })
+);
 const SortableSelect = SortableContainer(Select);
 const ReactSelect = (props) => {
-    const { className, showError = false, options, onChange, value, disabled = false, placeholder, isSearchable = true, isClearable, isMulti, isSortable, filterOption = undefined, } = props;
+    const {
+        className,
+        showError = false,
+        options,
+        onChange,
+        value,
+        disabled = false,
+        placeholder,
+        isSearchable = true,
+        isClearable,
+        isMulti,
+        isSortable,
+        filterOption = undefined
+    } = props;
     const classes = [
         className,
         'react-select',
-        showError && 'react-select--error',
-    ].filter(Boolean).join(' ');
+        showError && 'react-select--error'
+    ]
+        .filter(Boolean)
+        .join(' ');
+    console.log('options', options);
     const onSortStart = useCallback(({ helper }) => {
         const portalNode = helper;
         if (portalNode && portalNode.style) {
-            portalNode.style.cssText += 'pointer-events: auto; cursor: grabbing;';
+            portalNode.style.cssText +=
+                'pointer-events: auto; cursor: grabbing;';
         }
     }, []);
-    const onSortEnd = useCallback(({ oldIndex, newIndex }) => {
-        onChange(arrayMove(value, oldIndex, newIndex));
-    }, [onChange, value]);
+    const onSortEnd = useCallback(
+        ({ oldIndex, newIndex }) => {
+            onChange(arrayMove(value, oldIndex, newIndex));
+        },
+        [onChange, value]
+    );
     if (isMulti && isSortable) {
-        return (React.createElement(SortableSelect, { useDragHandle: true, 
+        return React.createElement(SortableSelect, {
+            useDragHandle: true,
             // react-sortable-hoc props:
-            axis: "xy", onSortStart: onSortStart, onSortEnd: onSortEnd, 
+            axis: 'xy',
+            onSortStart: onSortStart,
+            onSortEnd: onSortEnd,
             // small fix for https://github.com/clauderic/react-sortable-hoc/pull/352:
-            getHelperDimensions: ({ node }) => node.getBoundingClientRect(), 
+            getHelperDimensions: ({ node }) => node.getBoundingClientRect(),
             // react-select props:
-            placeholder: placeholder, ...props, value: value, onChange: onChange, disabled: disabled ? 'disabled' : undefined, className: classes, classNamePrefix: "rs", options: options, isSearchable: isSearchable, isClearable: isClearable, components: {
+            placeholder: placeholder,
+            ...props,
+            value: value,
+            onChange: onChange,
+            disabled: disabled ? 'disabled' : undefined,
+            className: classes,
+            classNamePrefix: 'rs',
+            options: options,
+            isSearchable: isSearchable,
+            isClearable: isClearable,
+            components: {
                 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
                 // @ts-ignore We're failing to provide a required index prop to SortableElement
                 MultiValue: SortableMultiValue,
                 MultiValueLabel: SortableMultiValueLabel,
-                DropdownIndicator: Chevron,
-            }, filterOption: filterOption }));
+                DropdownIndicator: Chevron
+            },
+            filterOption: filterOption
+        });
     }
-    return (React.createElement(Select, { placeholder: placeholder, ...props, value: value, onChange: onChange, disabled: disabled ? 'disabled' : undefined, components: { DropdownIndicator: Chevron }, className: classes, classNamePrefix: "rs", options: options, isSearchable: isSearchable, isClearable: isClearable, filterOption: filterOption }));
+    return React.createElement(Select, {
+        placeholder: placeholder,
+        ...props,
+        value: value,
+        onChange: onChange,
+        disabled: disabled ? 'disabled' : undefined,
+        components: { DropdownIndicator: Chevron },
+        className: classes,
+        classNamePrefix: 'rs',
+        options: options,
+        isSearchable: isSearchable,
+        isClearable: isClearable,
+        filterOption: filterOption
+    });
 };
 export default ReactSelect;
